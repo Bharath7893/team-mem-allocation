6b6484b528f8ac2cb6e70b0fdded1fc2f3cf52b3
Bharath7893 <bharathkumar7893@gmail.com>
2023-01-22 11:51:41 +0530

    modified header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = ()=>{
     return(
         <header>
-        <h1 className="py-4 text-center bg-primary text-white ">Header</h1>
+        <h1 className="py-4 text-center bg-primary text-white ">Header1</h1>
         </header>
     )
 }

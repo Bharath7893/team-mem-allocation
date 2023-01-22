e482f777968bc5a096b6ce517d087e10de53ccce
Bharath7893 <bharathkumar7893@gmail.com>
2023-01-22 11:48:40 +0530

    first commit team-mem-allocation

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = ()=>{
+    return(
+        <header>
+        <h1 className="py-4 text-center bg-primary text-white ">Header</h1>
+        </header>
+    )
+}
+
+export default Header
